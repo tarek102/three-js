a3bb8ebf28fdcc3d5c0b445fd1a97d484de095f9
unknown <tarekyosry8@gmail.com>
2023-08-07 02:19:53 +0300

    Modify Home

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,4 +1,10 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
+import { useSnapshot } from 'valtio'
+import config from '../config'
+import state from '../store'
+import { download } from '../assets'
+import 
 
 const Customizer = () => {
   return (
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { motion, AnimatePresence } from "framer-motion"
 import { snapshot, useSnapshot } from "valtio"
 import state from "../store"
+import { CustomButton } from "../components"
 import {
   headContainerAnimation,
   headContentAnimation,
@@ -36,7 +37,10 @@ const Home = () => {
               imagination</strong> {" "} and define your own style.
             </p>
             <CustomButton 
-            
+              type= "filled"
+              title= "Customize It"
+              handleClick= {() => state.intro = false}
+              customStyles= "w-fit px-4 py-2.5 font-bold text-sm"
             />
           </motion.div>
         </motion.section>
